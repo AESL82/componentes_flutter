e32320e6f31d3216f14477985ef77eb8a743bab4
Alex Segarra <alex.segarra1@gmail.com>
2021-05-02 20:17:35 -0400

    Fin 63 - Map de las listas y ListTile

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      //body: ListView(children: _crearIntems()),
+      body: ListView(children: _crearItemsCorta()),
+    );
+  }
+
+  // Retorno de lista de items a mostrar en el ListView. Método 1.
+  List<Widget> _crearIntems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  // Retorno de lista de items a mostrar en el ListView. Método 2.
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Texto de subtítulo de cada elemento.'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+}
