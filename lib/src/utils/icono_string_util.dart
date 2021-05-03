2754a41e0007c13d808cfb8248fcb76f70642d12
Alex Segarra <alex.segarra1@gmail.com>
2021-05-03 05:47:23 -0400

    68 - ícono por strings.

---
+++ lib/src/utils/icono_string_util.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// OJO: ícono que no aparezca en este mapa, simplemente no se va a mostrar.
+// Es el detalle a tener en consideración. Esto es temporal mientras se logra
+// obtener de Fluttter una opción ya propia en la clase Icon.
+final _icons = <String, IconData>{
+  'add_alert': Icons.add_alert,
+  'accessibility': Icons.accessibility,
+  'folder_open': Icons.folder_open,
+};
+
+Icon getIcon(String nombreIcono) {
+  return Icon(_icons[nombreIcono], color: Colors.blue);
+}
