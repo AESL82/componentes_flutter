19623d5e3fde179317e2bda3b9c2b7fcf7b0b15b
Alex Segarra <alex.segarra1@gmail.com>
2021-05-03 06:12:39 -0400

    69 - navegación a una nueva pantalla.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:componentes_flutter/src/providers/menu_provider.dart';
 import 'package:componentes_flutter/src/utils/icono_string_util.dart';
+import 'package:componentes_flutter/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -23,7 +24,7 @@ class HomePage extends StatelessWidget {
         //print(snapshot.data);
 
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
@@ -35,7 +36,7 @@ class HomePage extends StatelessWidget {
     */
   }
 
-  _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
@@ -44,7 +45,10 @@ class HomePage extends StatelessWidget {
         //leading: Icon(Icons.account_circle, color: Colors.blue),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
