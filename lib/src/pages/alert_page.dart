b1f070f5f3a271e3734da5f0601d38d259b70a95
Alex Segarra <alex.segarra1@gmail.com>
2021-05-04 08:40:12 -0400

    Widget - Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,14 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar Alertas'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blue, shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
         onPressed: () {
@@ -14,5 +22,35 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true, // Cerrar la alerta haciendo clik afuera.
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Título'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la caja de la alertas'),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Ok'))
+            ],
+          );
+        });
   }
 }
