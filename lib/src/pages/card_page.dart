0356087e1633c44fd00f371c0229b4acee5e5f5c
Alex Segarra <alex.segarra1@gmail.com>
2021-05-04 07:28:11 -0400

    Image y FadeIn - personalización de cards.

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,13 +9,24 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_cardTipo1()],
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+        ],
       ),
     );
   }
 
   Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -39,6 +50,46 @@ class CardPage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    final card = Container(
+      //clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          FadeInImage(
+            image: NetworkImage(
+                'https://pix4free.org/assets/library/2021-01-12/originals/winter_alps_mountains_landscape_clouds.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          /*
+          Image(
+              image: NetworkImage(
+                  'https://pix4free.org/assets/library/2021-01-12/originals/winter_alps_mountains_landscape_clouds.jpg')),
+          */
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Texto referente a la imagen.'))
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(borderRadius: BorderRadius.circular(30.0), child: card),
     );
   }
 }
