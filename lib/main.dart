df607f620a2c1a47488caba4d3ea98808385a31b
Alex Segarra <alex.segarra1@gmail.com>
2021-05-05 13:38:24 -0400

    Cambio de fecha, Datepicker.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:componentes_flutter/src/routes/routes.dart';
 import 'package:componentes_flutter/src/pages/alert_page.dart';
 
@@ -9,6 +10,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Componentes App',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', ''), // Spanish, no country code
+      ],
       debugShowCheckedModeBanner: false,
       //home: HomePage()
       initialRoute: '/',
