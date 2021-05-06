9ee108738b339c647a80568c2f346b723142dd4c
Alex Segarra <alex.segarra1@gmail.com>
2021-05-06 15:12:55 -0400

    InfiniteScroll.

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,7 +6,26 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+// Controlador de Scroller de la lista. Con esto podemos determinar cuando
+// lega al final de la lista para cargar luego más elementos.
+  ScrollController _scrollController = new ScrollController();
+
+//List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +39,7 @@ class _ListaPageState extends State<ListaPage> {
 
   _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemBuilder: (BuildContext context, int index) {
         final imagen = _listaNumeros[index];
 
@@ -30,5 +50,14 @@ class _ListaPageState extends State<ListaPage> {
       },
       itemCount: _listaNumeros.length,
     );
+  }
+
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+
+    setState(() {});
   }
 }
